60b342b92b26f33cc4e99181bfe696a4dd7e2f9d
Samuel Tallet <samuel.tallet@gmail.com>
2025-09-14 19:06:44 +0200

    Fix download On SU 2017

--- source/material_browser/download.rb
+++ source/material_browser/download.rb
@@ -21,6 +21,7 @@ raise 'The MBR plugin requires at least Ruby 2.2.0 or SketchUp 2017.'\
   unless RUBY_VERSION.to_f >= 2.2 # SketchUp 2017 includes Ruby 2.2.4.
 
 require 'open-uri'
+require 'openssl'
 require 'fileutils'
 
 # Material Browser plugin namespace.
@@ -44,17 +45,26 @@ module MaterialBrowser
 
       begin
         File.open(output_path, 'wb') do |output_file|
-          output_file.write(
-            URI.open(url, 'User-Agent' => USER_AGENT).read
-          )
+          stream = if RUBY_VERSION.to_f >= 2.5
+            URI.open(url, 'User-Agent' => USER_AGENT)
+          else
+            open(
+              url,
+              'User-Agent' => USER_AGENT,
+              :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE
+            )
+          end
+          output_file.write(stream.read)
           # Remarks:
+          # - > open-uri's Kernel.open will be deprecated in future.
+          # - URI.open() is callable since Ruby 2.5, else we fallback to open()
           # - User-Agent is required by some APIs, including Poly Haven one.
-          # - Seems HTTP redirects are not followed?
+          # - Seems SketchUp 2017 (Ruby 2.2.4) has trouble with modern SSL.
           # - Output file is overwritten if it already exists.
         end
       rescue => error
         FileUtils.remove_file(output_path) if File.exist?(output_path) # Cleanup
-        raise "Material Browser: Download failed with #{error.message} for #{url}"
+        raise "Download failed with #{error.message} for #{url}"
       end
     end
 
